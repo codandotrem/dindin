5f60f758336a465415b1087f008880a08ba8aa1e
chalestristian <thales.cristianeugenio@gmail.com>
2021-10-20 12:32:14 -0300

    Finalizando ListaCursosComponente

--- projeto-dindin/src/components/HeaderComponente.vue
+++ projeto-dindin/src/components/HeaderComponente.vue
@@ -1,11 +1,11 @@
 <template>
   <header>
     <div class="componentes-header-footer-container">
-      <a href="#"><img class="componentes-header-footer-logo-img" src="../assets/logo-dindin-white.png"></a>
+      <a href="#"><img class="componentes-header-footer-logo-img" src="../assets/logo-dindin-white.png"></a> <!-- link to index -->
       <h2 class="componentes-header-footer-title"><a href="#">dindin</a></h2>
         <div class="menu-header-conteiner">
-          <p class="menu-items"><a href="#">CURSOS</a></p>
-          <p class="menu-items"><a href="#">PAINEL ADMINISTRATIVO</a></p>
+          <p class="menu-items"><a href="#">CURSOS</a></p> <!-- link to cursos -->
+          <p class="menu-items"><a href="#">PAINEL ADMINISTRATIVO</a></p> <!-- link to painel adm -->
         </div>
     </div>
   </header>
